d9e685de8602d500c32d47edf52a942770c6d91c
bangmac <anhbexd@gmail.com>
2020-07-08 17:42:14 +0700

    comment code model

--- src/main/java/com/example/demo/repository/Repository.java
+++ src/main/java/com/example/demo/repository/Repository.java
@@ -4,18 +4,18 @@ public class Repository {
     private String abc;
 //    private String name;
     private int id;
-
-    public String getAbc() {
-        return abc;
-    }
-
-    public void setAbc(String abc) {
-        this.abc = abc;
-    }
-
-    public int getId() {
-        return id;
-    }
+//
+//    public String getAbc() {
+//        return abc;
+//    }
+//
+//    public void setAbc(String abc) {
+//        this.abc = abc;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
 
     public void setId(int id) {
         this.id = id;
