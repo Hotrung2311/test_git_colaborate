9dcca3ebdfc148edb2274f63eb37c14dcf4f3817
Hotrung2311 <trungho2311@gmail.com>
2020-07-08 17:11:01 +0700

    xoa name,them id va get setter

--- src/main/java/com/example/demo/repository/Repository.java
+++ src/main/java/com/example/demo/repository/Repository.java
@@ -2,5 +2,22 @@ package com.example.demo.repository;
 
 public class Repository {
     private String abc;
-    private String name;
+//    private String name;
+    private int id;
+
+    public String getAbc() {
+        return abc;
+    }
+
+    public void setAbc(String abc) {
+        this.abc = abc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
